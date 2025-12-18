bac122c40d9257e567c3cf57f35a65ed1fef4b21
bala-amypo <bala@amypo.in>
2025-12-18 04:26:59 +0000

    User commit

--- src/main/java/dto/AuthResponse.java
+++ src/main/java/dto/AuthResponse.java
@@ -1,3 +1,16 @@
-class public AuthResponse {
+package com.example.demo.dto;
 
+
+public class AuthResponse {
+private String token;
+
+
+public AuthResponse(String token) {
+this.token = token;
+}
+
+
+public String getToken() {
+return token;
+}
 }
