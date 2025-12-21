1153aa56dcbf251dc65779cb769314992047e39e
bala-amypo <bala@amypo.in>
2025-12-21 17:55:49 +0000

    User commit

---
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -0,0 +1,32 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.User;
+import com.example.demo.service.UserService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    private final UserService service;
+
+    public AuthController(UserService service) {
+        this.service = service;
+    }
+
+    @PostMapping("/register")
+    public User register(@RequestBody User user) {
+        return service.register(user);
+    }
+
+    @PostMapping("/login")
+    public User login(@RequestBody LoginRequest req) {
+        User user = service.findByEmail(req.getEmail())
+                .orElseThrow(() -> new RuntimeException("User not found"));
+
+        if (!user.getPassword().equals(req.getPassword())) {
+            throw new RuntimeException("Invalid password");
+        }
+        return user;
+    }
+}
