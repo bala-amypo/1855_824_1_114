a33ae47f7b83266fd31da5698c512a80490de5f8
bala-amypo <bala@amypo.in>
2025-12-21 17:26:01 +0000

    User commit

--- src/main/java/entity/User.java
+++ src/main/java/entity/User.java
@@ -1,88 +1,26 @@
+package com.example.demo.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
 
-package com.example.demo.model;
-import java.time.LocalDateTime;
-import jakarta.persistence.Entity;
-import jakarta.persistence.PrePersist;
-import jakarta.persistence.Id;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
 public class User {
-    @Id 
+
+    @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String employeeId;
-      private String fullName;
-      private String email;
-     private String teamName;
-     private String role;
-    private Boolean active = true;
-    private LocalDateTime createdAt;
 
-    @PrePersist
-    public void prePersist(){
-        this.createdAt = LocalDateTime.now();
-    }
-    
+    private String name;
 
-    public Long getId(){
-        return id;
-    }
-    public void setId(Long id){
-        this.id=id;
-    }
-    public String getEmployeeId(){
-        return employeeId;
-    }
-    public void setEmployeeId(String employeeId){
-        this.employeeId=employeeId;
-    }
-    public String getFullName(){
-        return fullName;
-    }
-    public void setFullName(String fullName){
-        this.fullName = fullName;
-    }
-    public String getEmail(){
-        return email;
-    }
-    public void setEmail(String email){
-        this.email = email;
-    }
-    public String getTeamName(){
-        return teamName;
-    }
-    public void setTeamName(String TeamName){
-        this.teamName = teamName;
-    }
-    public String getRole(){
-        return role;
-    }
-    public void setRole(String Role){
-        this.role = role;
-    }
-    public boolean getActive(){
-        return active;
-    }
-    public void setActive(Boolean active){
-        this.active = active;
-    }
-    public LocalDateTime getCreatedAt(){
-        return createdAt;
-    } 
-    public void setActiveLocalDateTime(LocalDateTime createdAt){
-        this.createdAt=createdAt;
-    } 
-    public EmployeeProfile(Long id, String employeeId, String fullName, String email, String teamName, String role, Boolean active, LocalDateTime createdAt){
-        this.id=id;
-        this.employeeId=employeeId;
-        this.fullName=fullName;
-        this.email=email;
-        this.teamName=teamName;
-        this.role=role;
-        this.active=active;
-        this.createdAt=createdAt;
-    }  
-    public EmployeeProfile(){}
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    private String password;
+
+    private String role;
 }
