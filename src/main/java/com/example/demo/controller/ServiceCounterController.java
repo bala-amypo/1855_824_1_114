a7436631d022644e07361d9d451b1dc3891d648e
bala-amypo <bala@amypo.in>
2025-12-22 05:12:45 +0000

    User commit

---
+++ src/main/java/com/example/demo/controller/ServiceCounterController.java
@@ -0,0 +1,35 @@
+package com.example.demo.controller;
+
+import com.example.demo.entity.ServiceCounter;
+import com.example.demo.service.ServiceCounterService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/counters")
+public class ServiceCounterController {
+
+    private final ServiceCounterService serviceCounterService;
+
+    public ServiceCounterController(ServiceCounterService serviceCounterService) {
+        this.serviceCounterService = serviceCounterService;
+    }
+
+
+    @PostMapping
+    public ResponseEntity<ServiceCounter> addCounter(@RequestBody ServiceCounter counter) {
+        return new ResponseEntity<>(
+                serviceCounterService.addCounter(counter),
+                HttpStatus.CREATED
+        );
+    }
+
+    
+    @GetMapping("/active")
+    public ResponseEntity<List<ServiceCounter>> getActiveCounters() {
+        return ResponseEntity.ok(serviceCounterService.getActiveCounters());
+    }
+}
